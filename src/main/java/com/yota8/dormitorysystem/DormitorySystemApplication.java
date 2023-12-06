a07377040ec7c765d6d015f6642405e1ad4ca59d
Yota8 <1602203689abcd@gmail.com>
2023-12-06 17:29:28 +0800

    Initial commit

---
+++ src/main/java/com/yota8/dormitorysystem/DormitorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.yota8.dormitorysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormitorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yota8/dormitorysystem/DormitorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yota8.dormitorysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
