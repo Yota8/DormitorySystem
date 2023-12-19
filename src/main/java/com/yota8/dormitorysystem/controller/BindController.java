de15c96df594ce3569b1cfbd4a65b77c1a7e2fb6
Yota8 <1602203689abcd@gmail.com>
2023-12-19 17:59:58 +0800

    Initial commit

--- src/main/java/com/yota8/dormitorysystem/controller/BindController.java
+++ src/main/java/com/yota8/dormitorysystem/controller/BindController.java
@@ -1,23 +1,23 @@
-package com.yota8.dormitorysystem.controller;
-
-
-import com.yota8.dormitorysystem.bean.BindBean;
-import com.yota8.dormitorysystem.bean.Result;
-import com.yota8.dormitorysystem.service.BindService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BindController {
-
-    @Autowired
-    private BindService bindService;
-
-    @PostMapping("/Bind")
-    public Result doBind(@RequestBody BindBean bindBean) {
-        return bindService.doBind(bindBean);
-    }
-
-}
+//package com.yota8.dormitorysystem.controller;
+//
+//
+//import com.yota8.dormitorysystem.bean.BindBean;
+//import com.yota8.dormitorysystem.bean.Result;
+//import com.yota8.dormitorysystem.service.BindService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class BindController {
+//
+//    @Autowired
+//    private BindService bindService;
+//
+//    @PostMapping("/Bind")
+//    public Result doBind(@RequestBody BindBean bindBean) {
+//        return bindService.doBind(bindBean);
+//    }
+//
+//}
--- src/main/java/com/yota8/dormitorysystem/service/Impl/BindServiceImpl.java
+++ src/main/java/com/yota8/dormitorysystem/service/Impl/BindServiceImpl.java
@@ -1,32 +1,32 @@
-package com.yota8.dormitorysystem.service.Impl;
-
-import com.yota8.dormitorysystem.bean.BindBean;
-import com.yota8.dormitorysystem.bean.Result;
-import com.yota8.dormitorysystem.mapper.BindMapper;
-import com.yota8.dormitorysystem.service.BindService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Objects;
-
-@Service
-public class BindServiceImpl implements BindService {
-
-    @Autowired
-    BindMapper bindMapper;
-
-    @Override
-    public Result doBind(BindBean bindBean) {
-        if (Objects.equals(bindBean.getTelephone(), "")) {
-            return new Result(0, "BIND_NO_MESSAGE", null);
-        }
-
-        int impactColumn = bindMapper.doBind(bindBean.getId(), bindBean.getPassword(), bindBean.getTelephone());
-
-        if (impactColumn >= 1) {
-            return new Result(1, "BIND_SUBMITTED", null);
-        } else {
-            return new Result(0, "BIND_NOT_SUBMITTED", null);
-        }
-    }
-}
+//package com.yota8.dormitorysystem.service.Impl;
+//
+//import com.yota8.dormitorysystem.bean.BindBean;
+//import com.yota8.dormitorysystem.bean.Result;
+//import com.yota8.dormitorysystem.mapper.BindMapper;
+//import com.yota8.dormitorysystem.service.BindService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Objects;
+//
+//@Service
+//public class BindServiceImpl implements BindService {
+//
+//    @Autowired
+//    BindMapper bindMapper;
+//
+//    @Override
+//    public Result doBind(BindBean bindBean) {
+//        if (Objects.equals(bindBean.getTelephone(), "")) {
+//            return new Result(0, "BIND_NO_MESSAGE", null);
+//        }
+//
+//        int impactColumn = bindMapper.doBind(bindBean.getId(), bindBean.getPassword(), bindBean.getTelephone());
+//
+//        if (impactColumn >= 1) {
+//            return new Result(1, "BIND_SUBMITTED", null);
+//        } else {
+//            return new Result(0, "BIND_NOT_SUBMITTED", null);
+//        }
+//    }
+//}
